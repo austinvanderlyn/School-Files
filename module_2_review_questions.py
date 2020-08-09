8c0c5dddb18e99d79dd8798c1edf315975e4dbc7
Austin Vanderlyn <69310589+austinvanderlyn@users.noreply.github.com>
2020-08-09 16:33:27 -0500

    Add files via upload

---
+++ module_2_review_questions.py
@@ -0,0 +1,10 @@
+# what is the syntax to obtain the first value of the tuple?
+A = ('a', 'b', 'c')
+print(A[0])
+
+# new dictionary
+Dict={"A":1,"B":"2","C":[3,3,3],"D":(4,4,4),'E':5,'F':6}
+
+# result of element D
+print(Dict['D'])
+
