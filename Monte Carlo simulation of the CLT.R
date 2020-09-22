1bbb815ac8683e4e451af9b588e4ef299943dd73
Austin Vanderlyn <69310589+austinvanderlyn@users.noreply.github.com>
2020-09-21 21:24:09 -0500

    Add files via upload

---
+++ Monte Carlo simulation of the CLT.R
@@ -0,0 +1,29 @@
+# Monte Carlo simulation using a set value of p
+p <-0.45
+N <- 1000
+
+# simulate one poll of size N and determin x_hat
+x <- sample(c(0,1), size = N, replace = TRUE, prob = c(1-p, p))
+x_hat <- mean(x)
+
+# simulate B polls of size N and determine average x_hat
+B <- 10000
+N <- 1000
+x_hat <- replicate(B, {
+  x <- sample(c(0,1), size = N, replace = TRUE, prob = c(1-p, p))
+  mean(x)
+})
+
+# histogram and QQ-plot of Monte Carlo results
+library(tidyverse)
+library(gridExtra)
+p1 <- data.frame(x_hat = x_hat) %>%
+  ggplot(aes(x_hat)) +
+  geom_histogram(binwidth = 0.005, color = "black")
+p2 <- data.frame(x_hat = x_hat) %>%
+  ggplot(aes(sample = x_hat)) +
+  stat_qq(dparams = list(mean = mean(x_hat), sd = sd(x_hat))) +
+  geom_abline() +
+  ylab("X_hat") +
+  xlab("Theoretical Normal") 
+grid.arrange(p1, p2, nrow = 1)
