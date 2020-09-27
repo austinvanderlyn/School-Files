eda36f92e1f3f5e520d552bb24e313820d1eb101
Austin Vanderlyn <69310589+austinvanderlyn@users.noreply.github.com>
2020-09-27 15:29:15 -0500

    Add files via upload

---
+++ Data-driven models.R
@@ -0,0 +1,16 @@
+# collect last result before the election for each pollster
+library(dslabs)
+one_poll_per_pollster <- polls %>% group_by(pollster) %>%
+  filter(enddate == max(enddate)) %>%
+  ungroup()
+
+# histogram of spread estimates
+one_poll_per_pollster %>%
+  ggplot(aes(spread)) +
+  geom_histogram(binwidth = 0.01)
+
+# construct 95% confidence interval
+results <- one_poll_per_pollster %>%
+  summarize(avg = mean(spread), se = sd(spread)/sqrt(length(spread))) %>%
+  mutate(start = avg - 1.96*se, end = avg + 1.96*se)
+round(results*100, 1)
