7f36b937cddb42f911aebe66cc275673adb8a0a0
Austin Vanderlyn <69310589+austinvanderlyn@users.noreply.github.com>
2020-11-05 19:05:32 -0600

    Add files via upload

---
+++ dates and times.R
@@ -0,0 +1,60 @@
+# inspect the startdate column of 2016 polls data, a Date type
+library(tidyverse)
+library(dslabs)
+data("polls_us_election_2016")
+polls_us_election_2016$startdate %>% head
+class(polls_us_election_2016$startdate) %>% head
+as.numeric(polls_us_election_2016$startdate) %>% head
+
+# ggplot is aware of dates
+polls_us_election_2016 %>% filter(pollster == "Ipsos" & state =="U.S.") %>%
+  ggplot(aes(startdate, rawpoll_trump)) +
+  geom_line()
+
+# lubridate: the tidyverse date package
+library(lubridate)
+
+# select some random dates from polls
+set.seed(2)
+dates <- sample(polls_us_election_2016$startdate, 10) %>% sort
+dates
+
+# extract month, day, year from date strings
+data.frame(date = dates,
+           month = month(dates),
+           day = day(dates),
+           year = year(dates))
+month(dates, label = TRUE)
+
+#ymd works on mixed dates styles
+x <- c(20090101, "2009-01-02", "2009 01 03", "2009-1-4",
+       "2009-1, 5", "Created on 2009 1 6", "200901 !!! 07")
+ymd(x)
+
+# different parsers extract year, month and day in different orders
+x <- "09/01/02"
+ymd(x)
+mdy(x)
+ydm(x)
+myd(x)
+dmy(x)
+dym(x)
+
+# current time in your time zone
+now()
+# current time in GMT
+now("GMT")
+# current hour
+now() %>% hour()
+# current minute
+now() %>% minute()
+# current second
+now() %>% second()
+
+# parse time
+x <- c("12:34:56")
+hms(x)
+
+# parse date and time
+x <- "Nov/2/2012 12:34:56"
+mdy_hms(x)
